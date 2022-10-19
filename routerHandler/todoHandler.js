ab4f4dfe5bfd0705e961dd728bab0ae645eb1c53
rasedul99 <rasedul2199@gmail.com>
2022-10-20 00:59:46 +0600

    post single data

--- routerHandler/todoHandler.js
+++ routerHandler/todoHandler.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const router = express.Router();
+const todoSchema = require("../schemas/todoSchemas");
+const Todo = new mongoose.model("Todo", todoSchema);
 
 // get all todos
 
@@ -8,7 +10,20 @@ router.get("/", (req, res) => {});
 // Get a todo By Id
 router.get("/:id", (req, res) => {});
 // Post Todo
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const newTodo = new Todo(req.body);
+  await newTodo.save((err) => {
+    if (err) {
+      res.status(500).json({
+        error: "there was server side error",
+      });
+    } else {
+      res.status(200).json({
+        message: "succesfully created",
+      });
+    }
+  });
+});
 // Post Mutiple Todos
 router.post("/all", (req, res) => {});
 // Put Todo
